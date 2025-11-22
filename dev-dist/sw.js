74efffdb4f340710b061b3b852ebb8f55efd8777
ngusadien <ngusa41@gmail.com>
2025-11-22 14:35:25 +0000

    v-1.0.4 cloudinary

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-13fdf7ef'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.pkjtqu0i2vg"
+    "revision": "0.vec8ed33tr"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
